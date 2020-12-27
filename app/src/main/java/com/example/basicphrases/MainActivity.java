6ac9e5b35e2367b90c2a9e4e6c20c0907a9dd100
prateek-ml <prateekb1912@gmail.com>
2020-12-27 15:08:20 +0530

    App completed

--- app/src/main/java/com/example/basicphrases/MainActivity.java
+++ app/src/main/java/com/example/basicphrases/MainActivity.java
@@ -2,10 +2,23 @@ package com.example.basicphrases;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void playPhrase(View view){
+        Button buttonPressed = (Button) view;
+
+        Log.i("Info", buttonPressed.getTag().toString());
+
+        MediaPlayer player = MediaPlayer.create(this, getResources().getIdentifier(buttonPressed.getTag().toString(), "raw", getPackageName()));
+
+        player.start();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
